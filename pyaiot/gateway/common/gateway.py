c238dcfb3b16fd9e1a7ebc374f8eed18a3e7eae9
YongSung Lee <yslee@humminglab.io>
2018-05-04 12:36:47 +0900

    fix to call future

--- pyaiot/gateway/common/gateway.py
+++ pyaiot/gateway/common/gateway.py
@@ -177,8 +177,8 @@ class GatewayBase(web.Application, GatewayBaseMixin, metaclass=ABCMeta):
         settings = {'debug': True}
 
         # Create connection to broker
-        self.create_broker_connection(
-            "ws://{}:{}/gw".format(options.broker_host, options.broker_port))
+        asyncio.ensure_future(self.create_broker_connection(
+            "ws://{}:{}/gw".format(options.broker_host, options.broker_port)))
 
         super().__init__(handlers, **settings)
         logger.debug('Base Gateway application started')
