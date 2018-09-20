41333a7d2f41e16108d990ec74d6d8deda995c3c
YongSung Lee <yslee@humminglab.io>
2018-09-20 12:35:47 +0900

    fix import path

--- pyaiot/gateway/coap/application.py
+++ pyaiot/gateway/coap/application.py
@@ -36,7 +36,8 @@ from tornado.options import define, options
 
 from pyaiot.common.auth import check_key_file
 from pyaiot.common.helpers import start_application, parse_command_line
-from pyaiot.gateway.common.gateway import CoapGateway, MAX_TIME, COAP_PORT
+from pyaiot.gateway.coap.gateway import CoapGateway, MAX_TIME, COAP_PORT
+
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)14s - '
