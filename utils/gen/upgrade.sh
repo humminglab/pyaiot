08ed4e8d8b0ac0da14f9152b161b179e555dc518
YongSung Lee <yslee@humminglab.io>
2018-09-21 20:22:31 +0900

    T76 stop script if return fail

--- utils/gen/upgrade.sh
+++ utils/gen/upgrade.sh
@@ -10,22 +10,31 @@ add_system_always()
   fi
 }
 
+set -e
+
 # increase file system
+echo 'Expand file system space'
 sudo systemctl enable armbian-resize-filesystem.service
 # re-enable ntpd for time synchronization
+echo 'Enable NTP server'
 sudo systemctl enable ntp.service
 
+echo 'Update git'
 cd /home/pi/project/pyaiot
 git fetch && git merge
 
 # add Restart field in system service
+echo 'Fix aiot service'
+set +e
 add_system_always /lib/systemd/system/aiot-broker.service
 add_system_always /lib/systemd/system/aiot-coap-gateway.service
 add_system_always /lib/systemd/system/aiot-dashboard.service
 add_system_always /lib/systemd/system/aiot-manager.service
+set -e
 sudo systemctl daemon-reload
 
-
+echo 'Sync'
 sync
 
+echo 'Reboot'
 sudo reboot
