af906900eb328f5f83103f5d110a8d99e5eed71a
YongSung Lee <yslee@humminglab.io>
2018-09-21 15:45:08 +0900

    T72 enable ntp service

--- utils/gen/upgrade.sh
+++ utils/gen/upgrade.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# increase file system
 sudo systemctl enable armbian-resize-filesystem.service
+# re-enable ntpd for time synchronization
+sudo systemctl enable ntp.service
+
 
 cd /home/pi/project/pyaiot
 git fetch && git merge
